ed4a7d4355fad674ef8bce3f119da787ffda44fe
tobinstultiens <tobin.stultiens@gmail.com>
2023-10-01 20:34:45 +0200

    Initial setup

---
+++ lua/custom/plugins/vim-pugins.lua
@@ -0,0 +1,18 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  "romainl/vim-cool",
+  "voldikss/vim-floaterm",
+  "norcalli/nvim-colorizer.lua",
+  "christoomey/vim-tmux-navigator",
+  {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
+}
