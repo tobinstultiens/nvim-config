8d8db8132019da9f1f5da3ce6bbc60e29373685c
tobinstultiens <tobin.stultiens@gmail.com>
2023-12-12 17:24:04 +0100

    Updated with neorg

---
+++ lua/custom/plugins/rust.lua
@@ -0,0 +1,12 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	"simrat39/rust-tools.nvim",
+        dependencies = {
+        "neovim/nvim-lspconfig",
+        "nvim-lua/plenary.nvim",
+        "mfussenegger/nvim-dap",
+  },
+}
