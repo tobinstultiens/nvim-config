e5292060f49f913a1dc3964b674c4ba8ba0a0b43
tobinstultiens <tobin.stultiens@gmail.com>
2024-01-07 17:21:51 +0100

    Removed workspaces

--- init.lua
+++ init.lua
@@ -295,11 +295,6 @@ local on_attach_lsp = function(_, bufnr)
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -524,8 +519,8 @@ require('which-key').register({
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
   ['<leader>f'] = { name = '[F]iles', _ = 'which_key_ignore' },
+  ['<leader>n'] = { name = '[N]eorg', _ = 'which_key_ignore' },
 })
 
 local neorgKeymap = { 
