6534b51c28d2e8d1f9f36bfcf9490512bfa1cd08
Tobin Stultiens <tobin.stultiens@gmail.com>
2026-02-09 23:20:36 +0100

    Updated packages

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -1,39 +1,34 @@
 return {
-    {
-        "vhyrro/luarocks.nvim",
-        priority = 1000, -- We'd like this plugin to load first out of the rest
-        config = true, -- This automatically runs `require("luarocks-nvim").setup()`
-    },
-    {
-        "nvim-neorg/neorg",
-        dependencies = { "luarocks.nvim", "nvim-lua/plenary.nvim" },
-        -- put any other flags you wanted to pass to lazy here!
-        config = function()
-            require("neorg").setup({
-              load = {
-                ["core.defaults"] = {},  -- Loads default behaviour
-                ["core.concealer"] = {
-                  config = {
-                    folds = true,
-                    icon_preset = "basic",
-                    -- init_open_folds = "never",
-                  }
-                }, -- Adds pretty icons to your documents
-                ["core.syntax"] = {}, -- Adds syntax to code blocks
-                ["core.completion"] = {
-                  config = {
-                    engine = "nvim-cmp"
-                  }
-                },                  -- Adds completion
-                ["core.dirman"] = { -- Manages Neorg workspaces
-                  config = {
-                    workspaces = {
-                      notes = "~/Documents",
-                    },
-                  },
-                },
+  {
+    'nvim-neorg/neorg',
+    dependencies = { 'luarocks.nvim', 'nvim-lua/plenary.nvim' },
+    -- put any other flags you wanted to pass to lazy here!
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {}, -- Loads default behaviour
+          ['core.concealer'] = {
+            config = {
+              folds = true,
+              icon_preset = 'basic',
+              -- init_open_folds = "never",
+            },
+          }, -- Adds pretty icons to your documents
+          ['core.syntax'] = {}, -- Adds syntax to code blocks
+          ['core.completion'] = {
+            config = {
+              engine = 'nvim-cmp',
+            },
+          }, -- Adds completion
+          ['core.dirman'] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = '~/Documents',
               },
-      })
-      end,
-    }
+            },
+          },
+        },
+      }
+    end,
+  },
 }
