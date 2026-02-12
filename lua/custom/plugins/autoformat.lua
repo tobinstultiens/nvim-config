2c825a7a0f7a1031f2aa3d4f379fa00c0342bf4d
Tobin Stultiens <tobin.stultiens@gmail.com>
2026-02-12 21:22:46 +0100

    Moved autoformat

---
+++ lua/custom/plugins/autoformat.lua
@@ -0,0 +1,73 @@
+-- autoformat.lua
+--
+-- Use your language server to automatically format your code on save.
+-- Adds additional commands as well to manage the behavior
+
+return {
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>cf',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[C]ode [F]ormat',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        ocaml = { 'ocamlformat' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },
+      },
+      formatters_injected = {
+        -- Set the options field
+        options = {
+          -- Set to true to ignore errors
+          ignore_errors = false,
+          -- Map of treesitter language to file extension
+          -- A temporary file name with this extension will be generated during formatting
+          -- because some formatters care about the filename.
+          lang_to_ext = {
+            bash = 'sh',
+            c_sharp = 'cs',
+            elixir = 'exs',
+            javascript = 'js',
+            julia = 'jl',
+            latex = 'tex',
+            markdown = 'md',
+            python = 'py',
+            ruby = 'rb',
+            rust = 'rs',
+            teal = 'tl',
+            r = 'r',
+            typescript = 'ts',
+            lua = 'lua',
+          },
+          -- Map of treesitter language to formatters to use
+          -- (defaults to the value from formatters_by_ft)
+          lang_to_formatters = {},
+        },
+      },
+    },
+  },
+}
